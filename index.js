253c49e4985f6723e97b94a3461ac09681dcdb72
Bella Zendejas <bella@zendejas.com>
2023-09-15 11:16:51 -0700

    Created forms for items and names
    Forms prompt users to enter items and their prices, and list the names of people who are splitting the bill.
    
    Added functions to sum the total of the items and list the updated total each time user submits new item

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-// Reading user's form input and displaying in result div
-document.getElementById("textForm").addEventListener("submit", function (event) {
+var sum = 0;
+
+// Enter Item Form
+document.getElementById("itemForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
+    var textInput = document.getElementById("textInput");
+    var priceInput = document.getElementById("priceInput");
+    var itemList = document.getElementById("itemList");
+    var total = document.getElementById("total");
 
     // Get the entered word from the input field
     var word = document.getElementById("textInput").value;
+    var price = document.getElementById("priceInput").value;
+    var listing = `${word} $${price}`;
 
-    // Display the entered word
-    document.getElementById("displayWord").textContent = word;
-
-    var orderedList = document.getElementById("orderedlist");
-
+    // Creating new li element, setting value to user input, and appending to ordered list
     var li = document.createElement("li");
-    li.setAttribute('id', word);
-    li.appendChild(document.createTextNode(word));
-    orderedList.appendChild(li);
-    console.log(orderedList);
+    li.appendChild(document.createTextNode(listing));
+    itemList.appendChild(li);
+
+    // Clear text input field
+    textInput.value = "";
+    priceInput.value = "";
+
+    // Call calculateSum and display total
+    calculateSum(price);
+    total.textContent = `$${sum}`;
 
 });
+
+
+function calculateSum(price) {
+    sum = Number(sum) + Number(price);
+    console.log(sum);
+}
+
+// Enter Name Form
+document.getElementById("nameForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    var textInput = document.getElementById("nameInput");
+    var itemList = document.getElementById("nameList");
+
+    // Get the entered word from the input field
+    var name = document.getElementById("nameInput").value;
+    var listing = `${name}`;
+
+    // Creating new li element, setting value to user input, and appending to ordered list
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(listing));
+    nameList.appendChild(li);
+
+    // Clear text input field
+    textInput.value = "";
+});
+
