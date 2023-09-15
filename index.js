8b7bbd28615a57b708372ed38bb14b102e350d62
Bella Zendejas <bella@zendejas.com>
2023-09-15 13:46:36 -0700

    Organized functions and added "remove" button
    "Remove" button will delete the last item in listing

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
+// Global variables
 var sum = 0;
+var itemArray = [];
+var itemList = document.getElementById("itemList");
 
-// Enter Item Form
+// Remove last item entered in array when button is clicked and update item list
+document.getElementById("removeLastItem").addEventListener("click", function (event) {
+    itemArray.splice(-1);
+    updateList();
+});
+
+// ITEMS FORM SUBMISSION: Update items & price listings with user input
 document.getElementById("itemForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
+
+    // Local variables
     var textInput = document.getElementById("textInput");
     var priceInput = document.getElementById("priceInput");
-    var itemList = document.getElementById("itemList");
     var total = document.getElementById("total");
-
-    // Get the entered word from the input field
     var word = document.getElementById("textInput").value;
     var price = document.getElementById("priceInput").value;
     var listing = `${word} $${price}`;
 
-    // Creating new li element, setting value to user input, and appending to ordered list
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(listing));
-    itemList.appendChild(li);
-
-    // Clear text input field
-    textInput.value = "";
-    priceInput.value = "";
-
-    // Call calculateSum and display total
+    // Add item to array, calculate new total, and update listing
+    addToArray(listing);
+    updateList();
     calculateSum(price);
     total.textContent = `$${sum}`;
 
+    // Clear text input field 
+    textInput.value = "";
+    priceInput.value = "";
 });
 
-
+// FUNCTION: Calculate sum 
 function calculateSum(price) {
     sum = Number(sum) + Number(price);
-    console.log(sum);
 }
 
-// Enter Name Form
+// FUNCTION: Add item to array
+function addToArray(item) {
+    itemArray.push(item);
+}
+
+// FUNCTION: Loop through array and update list with contents
+function updateList() {
+    itemList.textContent = "";
+    for (var i of itemArray) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(i));
+        itemList.appendChild(li);
+    }
+}
+
+// NAMES FORM SUBMISSION: Update names list with new input
 document.getElementById("nameForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
     var textInput = document.getElementById("nameInput");
