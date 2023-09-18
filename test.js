07f279f13c9634bc4108e5008a711a177a820020
Bella Zendejas <bella@zendejas.com>
2023-09-18 16:09:21 -0700

    idk

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+// Prefill item list when button is clicked for testing
 document.getElementById("prefill").addEventListener("click", function (event) {
     console.log("Button clicked");
+    addToArray("Bronx Food", 10);
+    updateList();
+    addToSum(10);
+    addToArray("Tuna", 20);
+    updateList();
+    addToSum(20);
+    addToArray("Bread", 30);
+    updateList();
+    addToSum(30);
 }); 
