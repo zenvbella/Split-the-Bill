8c4d4c33bb770890f402e2484e2cce88663751c8
Bella Zendejas <bella@zendejas.com>
2023-09-18 14:57:16 -0700

    update

--- test.js
+++ test.js
@@ -1,24 +1,3 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    var numberInput = document.getElementById("numberInput");
-    var calculateButton = document.getElementById("calculateButton");
-    var resultSpan = document.getElementById("result");
-
-    calculateButton.addEventListener("click", function () {
-        // Get the user-entered numbers as a string
-        var numbersString = numberInput.value;
-
-        // Split the string by comma and trim spaces
-        var numbersArray = numbersString.split(",").map(function (num) {
-            return parseFloat(num.trim());
-        });
-
-        // Calculate the sum
-        var totalSum = numbersArray.reduce(function (sum, num) {
-            return sum + (isNaN(num) ? 0 : num); // Check for NaN and treat it as 0
-        }, 0);
-
-        // Display the result
-        resultSpan.textContent = totalSum;
-    });
-});
+document.getElementById("prefill").addEventListener("click", function (event) {
+    console.log("Button clicked");
+}); 
